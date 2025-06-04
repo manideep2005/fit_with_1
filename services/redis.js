de17c4a284609d2bc88e383e1add5118468592f0
Manideep Gonugunta <manideepgonugunta@Manideeps-MacBook-Air.local>
2025-06-04 15:35:26 +0530

    hello

---
+++ services/redis.js
@@ -0,0 +1,8 @@
+const redis = require('redis');
+const client = redis.createClient({
+  url: process.env.REDIS_URL
+});
+
+client.on('error', (err) => console.log('Redis Client Error', err));
+
+module.exports = client;
